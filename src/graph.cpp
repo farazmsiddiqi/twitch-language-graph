f350ac29c1a62a4a83d496b34829ff8c5023c6fe
farazmsiddiqi <farazs27@gmail.com>
2021-12-10 11:50:09 -0600

    emerge from git hell a reborn phoenix (and finished graph class)

--- src/graph.cpp
+++ src/graph.cpp
@@ -59,7 +59,9 @@ Graph::Graph(string features_file, string edges_file) {
     }
 
     string edge_line, id;
-
+    // secondary maps
+    //unordered_map< int, Edge > tmp_map2;
+    //unordered_map< int, Edge > tmp_map1;
     // O(m) time
     bool is_on_edge_headers = true;
     while (getline(fin_edges, edge_line)) {
@@ -89,20 +91,38 @@ Graph::Graph(string features_file, string edges_file) {
         Node tmp_node_1 = data_map[node_ids[0]];
         Node tmp_node_2 = data_map[node_ids[1]];
         double weight = ((double) tmp_node_1.get_views() + (double) tmp_node_2.get_views()) / 2.0;
-
         Edge temp_edge1 = Edge(tmp_node_1, tmp_node_2, weight);
-        unordered_map< int, Edge > tmp_map1;
-        tmp_map1[node_ids[1]] = temp_edge1;
-        adj_list[node_ids[0]] = tmp_map1;
-        
-        Edge temp_edge2 = Edge(tmp_node_2, tmp_node_1, weight);
-        unordered_map< int, Edge > tmp_map2;
-        tmp_map2[node_ids[0]] = temp_edge2;
-        adj_list[node_ids[1]] = tmp_map2;
+
+        // tmp map is holding previous src's dest vals, and then getting added onto for current src vals
+        // tmp_map1[node_ids[1]] = temp_edge1;
+        // adj_list[node_ids[0]] = tmp_map1;
+
+        //unordered_map< int, unordered_map< int, Edge> > adj_list;
+       if (adj_list[node_ids[0]].size() == 0) {
+           unordered_map< int, Edge > tmp_map1;
+           tmp_map1.insert({node_ids[1], temp_edge1});
+           adj_list[node_ids[0]] = tmp_map1;
+       } else {
+           adj_list[node_ids[0]].insert({node_ids[1], temp_edge1});
+       }
+
+        /*
+        if adj_list[node_ids[0]] does not have a map:
+            make a tmp_map, add the first (int, Edge) pair to map
+        if adj_list[node_ids[0]] has a map already:
+            add another (int, Edge) pair to the map
+        */
+
+        // Edge temp_edge2 = Edge(tmp_node_2, tmp_node_1, weight);
+
+        // tmp_map2[node_ids[0]] = temp_edge2;
+        // adj_list[node_ids[1]] = tmp_map2;
 
         // num_id_1 follows num_id_2 on twitch
         
     }
+
+    std::cout << "exit graph ctor" << std::endl;
 
 }
 
@@ -112,14 +132,12 @@ void Graph::print_adj_list() {
     unordered_map< int, unordered_map< int, Edge> >::iterator it;
     for (it = adj_list.begin(); it != adj_list.end(); ++it) {
         unordered_map< int, Edge> map = it->second;
-        std::cout << it->first << " ";
+        std::cout << "src: " << it->first << std::endl;
         for (auto it2 = map.begin(); it2 != map.end(); ++it2) {
-            std::cout << it2->first << std::endl;
+            std::cout << " dest: " << it2->first << std::endl;
         }
     }
 }
-
-// make a print edges fn
 
 bool Graph::hasNode(int nodeID) {
     return data_map.find(nodeID) != data_map.end(); 
@@ -132,3 +150,6 @@ unordered_map<int, Node> Graph::getData_map() {
 unordered_map< int, unordered_map< int, Graph::Edge> > Graph::getAdj_list() {
     return adj_list;
 }
+
+// make a print edges fn
+
