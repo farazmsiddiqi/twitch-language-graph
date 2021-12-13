f38f25005d93d22d46e080b703764c068023b544
Shalin Mehta <snmehta2@siebl-0224-40.ews.illinois.edu>
2021-12-12 19:45:12 -0600

    added updates to readme for testing

---
+++ .objs/src/algorithm.d
@@ -0,0 +1,8 @@
+.objs/src/algorithm.o: src/algorithm.cpp src/algorithm.h src/graph.h \
+  src/node.h
+
+src/algorithm.h:
+
+src/graph.h:
+
+src/node.h:
---
+++ .objs/tests/test_graph_creation.d
@@ -0,0 +1,9 @@
+.objs/tests/test_graph_creation.o: tests/test_graph_creation.cpp \
+  tests/../cs225/catch/catch.hpp tests/../src/node.h \
+  tests/../src/graph.h
+
+tests/../cs225/catch/catch.hpp:
+
+tests/../src/node.h:
+
+tests/../src/graph.h:
